a123ef3727358804499ca30579b3c70dc9316654
Motion Team <team@xcally.com>
2016-07-19 18:19:51 +0200

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# openchannel-facebook
+# Facebook integration for your Motion Openchannel in Node.js
+
+## Overview
+
 The key to an effective customer service is focusing in the channels where your customers are. Know your customers and be accessible to them through the most important channels. It might be through Email, Chat, Facebook, Twitter or other social media platforms. It’s true that there are too many channels and you might think you need a lot of time and resource. Don’t worry! xCALLY Motion Open Channel enables you to develop your favorite channel in just few steps. You will be able to easily manage your customer interactions through any channel inside a single Omni channel Desktop interface. Let’s see how you can develop your favorite customer service channel.
 xCALLY Motion provides all the necessary tools to receive and send messages. All you need to do is implement a simple web service to exchange messages between xCALLY Motion server and your favorite channel. You can use any programming language you prefer.
+
+## Prerequisites
+
+  * [Node.js](http://nodejs.org/)
+  * [Git](http://git-scm.com/)
+
+
+## Setting up the app
+  * Download the code `git clone https://github.com/xcally/openchannel-facebook.git`
+  * Run `npm install` at the root folder to download dependencies.
+  * Run `node index.js` to start the application.
+  * Navigate to http://localhost:3000/ in your favourite browser.
+
+## Configuration
+
+Please see `config.json` in the root folder if you want to change the default application / mongo settings.
+
+xCALLY Motion | Powered by Xenialab s.r.l. - [www.xcallymotion.com](https://www.xcallymotion.com/)
---
+++ index.js
@@ -0,0 +1,94 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    login = require('facebook-chat-api'),
+    request = require('request-promise'),
+    Promise = require('bluebird'),
+     fs = require('fs');
+
+var app = express();
+
+// Configuration
+try {
+    var configJSON = fs.readFileSync(__dirname + '/config.json');
+    var config = JSON.parse(configJSON.toString());
+} catch (e) {
+    console.error('File config.json not found or is invalid: ' + e.message);
+    process.exit(1);
+}
+
+var port = config.port || 3000;
+
+//bodyParser to get POST parameters.
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+//Start server
+app.listen(port, function() {
+    console.log('Service listening on port ' + port);
+});
+
+
+function startapi(api) {
+    console.log(api);
+    //listen for new facebook messages
+    api.listen(function(err, message) {
+        if (err) return console.error(err);
+
+        console.log(message);
+        var getUserInfo = Promise.promisify(api.getUserInfo);
+        // Get information (Name) about the sender
+        getUserInfo(message.senderID)
+            .then(function(userInfo) {
+                // Request body
+                var data = {
+                    from: message.senderID,
+                    body: message.body,
+                    name: userInfo[message.senderID].name
+                };
+                // HTTP request (Forward message to Motion)
+                return request({
+                    method: 'POST',
+                    uri: config.url,
+                    body: data,
+                    json: true
+                });
+            })
+            .then(function(res) {
+                console.log('request sent', res);
+            })
+            .catch(function(err) {
+                console.log('err', err);
+            });
+
+    });
+    //  Send message
+    //http://localhost:3000/sendMessage
+    app.post('/sendMessage', function(req, res) {
+        var recipientId = req.body.to;
+        var message = req.body.body;
+        // Send message to recipient
+        api.sendMessage(message, recipientId, function(err, data) {
+            if (err) {
+                console.log('Message failed', err);
+                res.status(500).send(err);
+            }
+            if (data) {
+                console.log('Message sent');
+                res.status(200).send(data);
+            }
+
+        });
+
+    });
+
+}
+//Login to facebook account
+login({
+    email: config.email,
+    password: config.password
+}, function(err, api) {
+    if (err) return console.error(err);
+    startapi(api);
+});
